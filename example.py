e6aeada8e397a59df1c87e07cde753e923e0b524
LordYusei <Spiritxero@gmail.com>
2015-12-23 23:15:44 +0000

    Updates
    Added User ranks, chain of command, xp, cash and level, more to come
    soon

--- example.py
+++ example.py
@@ -11,9 +11,49 @@ print("[INFO][DATA]Loading Room list...")
 time.sleep(1)
 for name in f.readlines():
     if len(name.strip())>0: groups.append(name.strip())
-    f.close()
-    print("Rooms successfully loaded...")
+f.close()
+print("Rooms successfully loaded...")
 
+whitelist = []
+f = open("filesystem/groupvariables/whitelist.txt", "r")
+print("[INFO][DATA]Loading Whitelist...")
+time.sleep(1)
+for name in f.readlines():
+    if len(name.strip())>0: whitelist.append(name.strip())
+f.close()
+print("whitelist successfully loaded...")
+
+Masters = []
+f = open("filesystem/groupvariables/Masters.txt", "r")
+print("[INFO][DATA]Loading Masters...")
+time.sleep(1)
+for name in f.readlines():
+    if len(name.strip())>0: Masters.append(name.strip())
+f.close()
+print("Masters successfully loaded...")
+
+HalfM = []
+f = open("filesystem/groupvariables/HalfMasters.txt", "r")
+print("[INFO][DATA]Loading Half Masters...")
+time.sleep(1)
+for name in f.readlines():
+    if len(name.strip())>0: HalfM.append(name.strip())
+f.close()
+print("HalfMasters successfully loaded...")
+
+##Ranking System
+ranks=dict()
+f = open("filesystem/Games/UserVariables.txt", "r") # read-only
+print("[INFO]LOADING Ranking Structure...")
+time.sleep(1)
+for line in f.readlines():
+  try:
+    if len(line.strip())>0:
+      user, exp,lvl, money= json.loads(line.strip())
+      ranks[user] = json.dumps([exp,lvl,money])
+  except:
+    print("[ERROR]Cant load Rank: %s" % line)
+f.close()
 nicks = dict() 
 f = open("filesystem/groupvariables/nicks.txt", "r") # read-only
 print("[INFO][DATA]Loading Nicknames...")
@@ -26,7 +66,6 @@ for line in f.readlines():
   except:
     print("[ERROR]Cant load nick: %s" % line)
 f.close()
-print("Nicknames successfully Loaded...")
 
 class Bot(chlib.ConnectionManager):
                                 
@@ -38,8 +77,11 @@ class Bot(chlib.ConnectionManager):
                                                 self.nColor = "8B6914"
 
                                 def getAccess(self, user):
-                                                if self.name == "LordYusei": return 4 #Just a rank option. You can edit this to allow Whitelisted users, etc
-                                                if self.name == "AddYourNameHere": return 4
+                                                if self.name == "denisstoff": return 4 #Just a rank option. You can edit this to allow Whitelisted users, etc
+                                                elif self.name == "AddYourNameHere": return 4
+                                                elif user.lower() in Masters: return 3
+                                                elif user.lower() in HalfM: return 2
+                                                elif user.lower() in whitelist: return 1
                                                 else: return 0
                                 def recvdenied(self, group):
                                                 print("Failed to connect to "+group.name)
@@ -59,8 +101,22 @@ class Bot(chlib.ConnectionManager):
 
                                 def recvPost(self, group, user, post):
                                                 print(user+": "+post.post)
-                                                if post.post.startswith("ayytron") or post.post.startswith("ayy") or post.post.startswith("tron"):
-                                                                group.sendPost("Yesh? o.o")
+                                                def AddStarterCash():
+                                                    if user not in ranks:
+                                                        exp=0
+                                                        lvl=1
+                                                        money=2000
+                                                        money=int(money)
+                                                        lvl=int(lvl)
+                                                        exp=int(exp)
+                                                        ranks[user] = json.dumps([exp,lvl,money])
+                                                        print("User added to ranks successfully")
+                                                    else:
+                                                        print("Failed to add user to money")
+                                                if post.post.startswith("YourBotNameHere") or post.post.startswith("AddShortNameHere") or post.post.startswith("AddAnotherShortNameHere"):
+                                                                if user.lower() == "YourBotNameHere": return
+                                                                else:
+                                                                            group.sendPost("Yesh? o.o")
                                                 if post.post.startswith("Airstrike"):
                                                                 if group.getAuth(user) > 0:
                                                                             group.clearGroup()
@@ -95,6 +151,43 @@ class Bot(chlib.ConnectionManager):
                                                                                                         uploader = d["snippet"]["channelTitle"]
                                                                                                         group.sendPost("I found: "+link+" <b>"+title+"</b> Posted By: "+uploader+"")
                                                                                                         return
+                                                                                  if cmd == "wl":
+                                                                                                        user=user.lower()
+                                                                                                        AddStarterCash()
+                                                                                                        f = open("filesystem/groupvariables/whitelist.txt", "a")
+                                                                                                        f.write("\n"+user)
+                                                                                                        f.close()
+                                                                                                        group.sendPost("Congratulations! You have been added to the whitelist and have now gained access to all of the commands a whitelisted user can use! [+2000$]")
+                                                                                  if cmd == "lvl":
+                                                                                                        user = user.lower()
+                                                                                                        if user in ranks:
+                                                                                                                exp,lvl,money = json.loads(ranks[user])
+                                                                                                                lvl=str(lvl)
+                                                                                                                exp=int(exp)
+                                                                                                                nextlevel = 1000
+                                                                                                                nextlevel = int(nextlevel)
+                                                                                                                neededxp = nextlevel - exp
+                                                                                                                neededxp=str(neededxp)
+                                                                                                                group.sendPost(user+" You are currently Level "+str(lvl)+" You need "+str(neededxp)+" To reach the next level")
+                                                                                                        else:
+                                                                                                                group.sendPost("I'm sorry, you aren't in the ranks database. Try typing .wl again")
+                                                                                  if cmd == "bal":
+                                                                                                        user=user.lower()
+                                                                                                        if user in ranks:
+                                                                                                                exp,lvl,money = json.loads(ranks[user])
+                                                                                                                money=str(money)
+                                                                                                                group.sendPost(user+" You currently have "+str(money)+ "$ available in your piggy bank")
+                                                                                                        else:
+                                                                                                                group.sendPost("I'm sorry, you aren't in the ranks database. Try typing .wl again")
+                                                                                  if cmd == "sav":
+                                                                                                        print("[SAVE] SAVING Ranking Data...")
+                                                                                                        f = open("filesystem/Games/UserVariables.txt", "w")
+                                                                                                        for user in ranks:
+                                                                                                                        exp,lvl,money = json.loads(ranks[user])
+                                                                                                                        f.write(json.dumps([user, exp,lvl,money])+"\n")
+                                                                                                        f.close()
+                                                                                                        group.sendPost("I have saved all data to the necessary files")
+
                                                                                   if cmd == "img" or cmd == "gis" and len(args) > 0:
                                                                                                         try:
                                                                                                                 search = args.replace(" ","_")
@@ -149,8 +242,6 @@ class Bot(chlib.ConnectionManager):
                                                                                                                         group.sendPost(random.choice(["http://img12.deviantart.net/de2a/i/2014/102/a/d/hatsune_miku_ecchi_render__2_by_annechan34-d7e5obz.png","http://img10.deviantart.net/6b0d/i/2012/091/3/3/ecchi_render_by_katkoyox-d4un1x2.png","http://k41.kn3.net/taringa/2/0/2/8/2/8/13/takemikazuchi2/7B3.jpg?3236","http://orig12.deviantart.net/7e25/f/2013/178/4/2/ecchi_by_hsalacard-d6av9ie.jpg","http://www.renders-graphiques.fr/image/upload/normal/Ecchi_girl.png","http://orig15.deviantart.net/8571/f/2013/280/a/1/render_girl_anime_ecchi_by_arihirokushinada-d6pk17d.png"]))
                                                                                                         else:
                                                                                                                         group.sendPost("I am not allowed to post this here")
-                                                                                  if cmd == "test":
-                                                                                                        group.sendPost("this part will get sent<b>and this part won't</b>")
                                 def recvmsg(self, group, user, pm):
                                                 print("PM: "+user+": "+pm)
                                                 if pm.startswith("Axuf") or pm.startswith("axuf"):
@@ -198,5 +289,5 @@ class Bot(chlib.ConnectionManager):
                                                 self.addGroup(group.name)
 
 if __name__ == "__main__": #no easy starting this time ;D
-                                bot = Bot(user = "AddYourUsername", password = "AddYourPassword", pm = True)
+                                bot = Bot(user = "ayytron", password = "D3vModding", pm = True)
                                 bot.main()
